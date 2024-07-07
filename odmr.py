7616f880c58154b10e6b2acab29e21659cb7459e
Handsoup <szasz.bence2000@gmail.com>
2024-07-07 22:22:41 +0200

    update

--- odmr.py
+++ odmr.py
@@ -31,6 +31,19 @@ def rot(n1, n2):
         rotation_matrix = c * diag + s * asym + (1 - c) * np.outer(u, u)
         return rotation_matrix
 
+
+def cartesian_to_spherical(x, y, z):
+    # Compute the radial distance
+    r = math.sqrt(x**2 + y**2 + z**2)
+
+    # Compute the polar angle (theta)
+    theta = math.acos(z / r)
+
+    # Compute the azimuthal angle (phi)
+    phi = math.atan2(y, x)
+
+    return theta, phi
+
 # Spin operators
 s_z = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1]])
 s_y = np.array([[0, -1j, 0], [1j, 0, -1j], [0, 1j, 0]]) / np.sqrt(2.0)
@@ -58,8 +71,11 @@ g = float(2.0026)
 
 # Directions
 n_z = np.array([0, 0, -1])
-theta = 0.95531661812450927816385710251576
-phi = 0.78539816
+#n_z = np.array([0, 1, 0])
+#theta = 0.95531661812450927816385710251576
+#phi = 0.78539816
+name = "Figure_5_100_at"
+theta, phi = cartesian_to_spherical(1,0,0)
 direction = (np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta))
 
 dir1 = np.array([1, -1, -1]) / np.sqrt(3)
@@ -93,7 +109,7 @@ for diri in [dir1, dir2, dir3, dir4]:
       # eigvectors0 = np.append(eigvectors0, np.linalg.norm(eigvect[:, 0]))
       # eigvectors1 = np.append(eigvectors1, np.linalg.norm(eigvect[:, 1]))
       # eigvectors2 = np.append(eigvectors2, np.linalg.norm(eigvect[:, 2]))
-      
+
       
 s1=np.array(s_1).reshape(4,len(B))
 s2=np.array(s_2).reshape(4,len(B))
@@ -103,3 +119,72 @@ plt.figure(figsize=(12, 8))
 plt.plot(B, s1[0], label=f's1')
 plt.plot(B, s2[0], label=f's2')
 plt.plot(B, s3[0], label=f's3')
+plt.xlim(0, 5000)
+plt.ylim(-1,1)
+plt.savefig(name + "0_short.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[0], label=f's1')
+plt.plot(B, s2[0], label=f's2')
+plt.plot(B, s3[0], label=f's3')
+plt.savefig(name + "0_full.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[1], label=f's1')
+plt.plot(B, s2[1], label=f's2')
+plt.plot(B, s3[1], label=f's3')
+plt.xlim(0, 5000)
+plt.ylim(-1,1)
+plt.savefig(name + "1_short.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[1], label=f's1')
+plt.plot(B, s2[1], label=f's2')
+plt.plot(B, s3[1], label=f's3')
+plt.savefig(name + "1_full.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[2], label=f's1')
+plt.plot(B, s2[2], label=f's2')
+plt.plot(B, s3[2], label=f's3')
+plt.xlim(0, 5000)
+plt.ylim(-1,1)
+plt.savefig(name + "2_short.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[2], label=f's1')
+plt.plot(B, s2[2], label=f's2')
+plt.plot(B, s3[2], label=f's3')
+plt.savefig(name + "2_full.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[3], label=f's1')
+plt.plot(B, s2[3], label=f's2')
+plt.plot(B, s3[3], label=f's3')
+plt.xlim(0, 5000)
+plt.ylim(-1,1)
+plt.savefig(name + "3_short.png")
+plt.show()
+plt.close()
+
+plt.figure(figsize=(12, 8))
+plt.plot(B, s1[3], label=f's1')
+plt.plot(B, s2[3], label=f's2')
+plt.plot(B, s3[3], label=f's3')
+plt.savefig(name + "3_full.png")
+plt.show()
+plt.close()
+
+
